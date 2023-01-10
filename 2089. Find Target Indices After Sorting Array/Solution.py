f084ba47f4afe7ad9693d5381dee68f61bfb6be1
Datsede04 <datsede04@gmail.com>
2023-01-10 23:46:48 +0300

    Runtime 151 ms Beats 5.12% Memory 13.8 MB Beats 65.50%

---
+++ 2089. Find Target Indices After Sorting Array/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            for j in range(len(nums)-1):
+                if nums[j] > nums[j+1]:
+                    temp = nums[j]
+                    nums[j] = nums[j+1]
+                    nums[j+1] = temp
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                result.append(i)
+
+        return result        
+             
