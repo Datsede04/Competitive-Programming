1fba1bcaa14b3c5f76358d644d890165d5d92d9d
Datsede04 <datsede04@gmail.com>
2023-02-10 00:25:39 +0300

    https://leetcode.com/problems/longest-substring-without-repeating-characters

--- 3. Longest Substring Without Repeating Characters/python.py
+++ 3. Longest Substring Without Repeating Characters/python.py
@@ -4,9 +4,9 @@ class Solution:
         l = 0
         res = 0
         for r in range(len(s)):
-            while(s[r] in carSet):
-                carSet.remove(s[r])
+            while s[r] in carSet:
+                carSet.remove(s[l])
                 l+=1
             carSet.add(s[r])  
             res = max(res,r-l+1)
-        return res
+        return res  
